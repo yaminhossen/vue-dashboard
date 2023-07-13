7d8d474a10e77e41a6a470fb2eb578ac2e4d8444
yaminhossen <streatdot616@gmail.com>
2023-07-13 14:17:39 +0600

    create page done

--- src/pages/private/pages/user_management/Detailss.vue
+++ src/pages/private/pages/user_management/Detailss.vue
@@ -1,5 +1,100 @@
 <template>
-  <div>This is user details</div>
+  <div class="user_create">
+    <div class="create_title">
+      <span class="create">Create</span>
+      <span class="back_button">
+        <span>
+          <i class="fa-solid fa-arrow-left"></i>
+        </span>
+        <span>Back</span>
+      </span>
+    </div>
+    <div class="users_details">
+      <div class="user_details">
+        <div class="user_details_title">
+          <img src="/duser.png" alt="">
+          <span>User Details</span>
+        </div>
+      </div>
+      <div>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>info</th>
+            </tr>
+          </thead>
+          <tbody class="table-border-bottom-0">
+            <tr>
+              <td>
+                Id
+              </td>
+              <td>01</td>
+            </tr>
+            <tr>
+              <td>
+                First Name
+              </td>
+              <td>Super</td>
+            </tr>
+            <tr>
+              <td>
+                Last Name
+              </td>
+              <td>Admin</td>
+            </tr>
+            <tr>
+              <td>
+                User Name
+              </td>
+              <td>Super Admin</td>
+            </tr>
+            <tr>
+              <td>
+                Email
+              </td>
+              <td>abc@gmail.com</td>
+            </tr>
+            <tr>
+              <td>
+                Mobile Number
+              </td>
+              <td>01789-653476</td>
+            </tr>
+            <tr>
+              <td>
+                Slug
+              </td>
+              <td>Admin</td>
+            </tr>
+            <tr>
+              <td>
+                Status
+              </td>
+              <td>Admin</td>
+            </tr>
+            <tr>
+              <td>
+                Create At
+              </td>
+              <td>Somethings</td>
+            </tr>
+            <tr>
+              <td>
+                Updated At
+              </td>
+              <td>Somethings</td>
+            </tr>
+          </tbody>
+
+        </table>
+      </div>
+      <div class="create_border"></div>
+      <div class="create_submit">
+        <div><i class="fa-solid fa-upload"></i>Submit</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +103,4 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
